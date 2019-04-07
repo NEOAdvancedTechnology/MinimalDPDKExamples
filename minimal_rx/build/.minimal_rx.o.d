1792cf98ce45b16d422d0342f95cffa4cffc9863
Thomas Edwards <root@ip-172-30-0-244.ec2.internal>
2019-04-07 03:59:07 +0000

    cleaned up old rx file

---
+++ minimal_rx/build/.minimal_rx.o.d
@@ -0,0 +1,120 @@
+dep_minimal_rx.o = /home/ec2-user/MinimalDPDKExamples/minimal_rx/minimal_rx.c \
+ /usr/include/stdc-predef.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /usr/include/stdio.h /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/gnu/stubs.h \
+ /usr/include/gnu/stubs-64.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stddef.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/libio.h /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdarg.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/wchar.h /usr/include/inttypes.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_eal.h \
+ /usr/include/sched.h /usr/include/time.h /usr/include/bits/sched.h \
+ /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/xlocale.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_config.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_compat.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_common.h \
+ /usr/include/stdlib.h /usr/include/bits/waitflags.h \
+ /usr/include/bits/waitstatus.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/byteswap.h \
+ /usr/include/bits/byteswap-16.h /usr/include/sys/types.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/sigset.h /usr/include/sys/sysmacros.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-float.h /usr/include/ctype.h \
+ /usr/include/errno.h /usr/include/bits/errno.h \
+ /usr/include/linux/errno.h /usr/include/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/posix1_lim.h \
+ /usr/include/bits/local_lim.h /usr/include/linux/limits.h \
+ /usr/include/bits/posix2_lim.h /usr/include/bits/xopen_lim.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_per_lcore.h \
+ /usr/include/pthread.h /usr/include/bits/setjmp.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_bus.h \
+ /usr/include/sys/queue.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_log.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_dev.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_pci_dev_feature_defs.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_ethdev.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_interrupts.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_eal_interrupts.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_devargs.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_errno.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_ether.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_memcpy.h \
+ /usr/include/string.h /usr/include/bits/string.h \
+ /usr/include/bits/string2.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_vect.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_vect.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/mm_malloc.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/immintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xtestintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/rdseedintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/prfchwintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xsaveintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/xsaveoptintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/adxintrin.h \
+ /usr/lib/gcc/x86_64-redhat-linux/4.8.5/include/pkuintrin.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_random.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_mbuf.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_mempool.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_spinlock.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_spinlock.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_lcore.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_launch.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_pause.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_pause.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_rtm.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_cpuflags.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_cpuflags.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_branch_prediction.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_common.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_pause.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_cycles.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_cycles.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_debug.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_log.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_atomic.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_atomic.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_atomic_64.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_branch_prediction.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_memory.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_ring.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_memzone.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_ring_generic.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_prefetch.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_prefetch.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_mbuf_ptype.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_byteorder.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/generic/rte_byteorder.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_byteorder_64.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_eth_ctrl.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_dev_info.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_ethdev_core.h \
+ /home/ec2-user/dpdk-18.11/x86_64-native-linuxapp-gcc/include/rte_cycles.h
