2a78312ef22f45fda8d41cfd19f1635c4f7acd5f
Thomas Edwards <thomas.edwards@fox.com>
2019-04-25 23:36:24 +0000

    cleaned up comments

--- minimal_rx/minimal_rx.c
+++ minimal_rx/minimal_rx.c
@@ -122,13 +122,8 @@ void rx_packets(void)
 
 	/* Run until the application is quit or killed. */
 	for (;;) {
-		/*
-		 * Receive packets on a port and forward them on the paired
-		 * port. The mapping is 0 -> 1, 1 -> 0, 2 -> 3, 3 -> 2, etc.
-		 */
 		RTE_ETH_FOREACH_DEV(port) {
 
-			/* Get burst of RX packets, from first port of pair. */
 			struct rte_mbuf *bufs[BURST_SIZE];
 			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
 					bufs, BURST_SIZE);
