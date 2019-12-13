b2764834a63f3cefd8d3fb722604039a13ff3486
Networks Engineering & Operations Advanced Technology (Disney) <thomas.edwards@fox.com>
2019-12-12 17:24:41 -0800

    Update minimal_rx.c

--- minimal_rx/minimal_rx.c
+++ minimal_rx/minimal_rx.c
@@ -144,9 +144,9 @@ void rx_packets(void)
 
 			for(i=0;i<nb_rx;++i){
 
-				//printf("----->processing packet %d\n",i);
-				//printf("----->pkt_len=%d\n",bufs[i]->pkt_len);
-				//DumpHex(rte_pktmbuf_mtod(bufs[i],char *),bufs[i]->pkt_len);
+				printf("----->processing packet %d\n",i);
+				printf("----->pkt_len=%d\n",bufs[i]->pkt_len);
+				DumpHex(rte_pktmbuf_mtod(bufs[i],char *),bufs[i]->pkt_len);
 
 				rte_pktmbuf_free(bufs[i]);
 			}
